baa610623c3d9c7aa8f03aa63dacda03c3ccd85b
willactual <61017272+willactual@users.noreply.github.com>
2020-04-27 14:57:07 -0500

    Update coords.js

--- coords.js
+++ coords.js
@@ -1,5 +1,5 @@
 let i = document.querySelector("img");
-i.addEventListener("click", function() {
+i.addEventListener("click", function(evt) {
   let newX = evt.clientX - i.x;
   let newY = evt.clientY - i.y;
   alert("x: " + newX + ", y: " + newY);
